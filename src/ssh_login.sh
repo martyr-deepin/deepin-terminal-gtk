b9dee0df0838ee36cf7017e1dd1e539555ff999a
Xu FaSheng <fasheng.xu@gmail.com>
2013-10-19 16:11:15 +0800

    Update ssh_login.sh, fix connection issue if use ssh private/public key

--- src/ssh_login.sh
+++ src/ssh_login.sh
@@ -1,28 +1,48 @@
-#!/usr/bin/expect -f  
-set user [lindex $argv 0 ]
-set server [lindex $argv 1 ]
-set password [lindex $argv 2 ]
-set port [lindex $argv 3 ]
-set timeout 10
-#puts $user
-#puts $server
-#puts $password
-#puts $port
-if { ![string compare $port "" ] == 0 } { spawn ssh $user@$server -p $port } else { spawn ssh $user@$server };
+#! /usr/bin/expect -f
 
-if { ![string compare $password ""] == 0 } {
-    #puts "PASSWORD GIVEN!"
+## All possible interactive messages:
+# Are you sure you want to continue connecting (yes/no)?
+# password:
+# Enter passphrase for key
+
+## Main
+if {$argc < 2} {
+    set scriptname [lindex [split $argv0 "/"] end]
+    send_user "Usage: $scriptname <user> <host>\n"
+    send_user "Environment variables: EXP_SSH_PASS, EXP_SSH_PORT, EXP_SSH_OPT\n"
+    exit 1
+}
+
+# Setup variables
+set timeout 10
+set user [lindex $argv 0]
+set host [lindex $argv 1]
+set ssh_cmd "ssh"
+set ssh_opt "$user@$host"
+
+set password ""
+if {[info exists env(EXP_SSH_PASS)]} {
+    set password $env(EXP_SSH_PASS)
+}
+
+if {[info exists env(EXP_SSH_PORT)]} {
+    set ssh_opt "$ssh_opt -p $env(EXP_SSH_PORT)"
+}
+
+if {[info exists env(EXP_SSH_OPT)]} {
+    set ssh_opt "$ssh_opt $env(EXP_SSH_OPT)"
+}
+
+# Spawn and expect
+eval spawn $ssh_cmd $ssh_opt
+if {[string length $password]} {
     expect {
-        "*yes/no" { send "yes\r"; exp_continue}
-        "*password:" { send "$password\r" }
-        exp_continue
-    }
-} else {
-    expect {
-        "*yes/no" { send "yes\r"; exp_continue }
-        exp_continue
+        timeout {send_user "ssh connection time out, please operate manually\n"}
+        -nocase "(yes/no)\\?" {send "yes\r"; exp_continue}
+        -nocase -re "password:|enter passphrase for key" {
+            send "$password\r"
+        }
     }
 }
 
-#puts "interacting"
 interact
