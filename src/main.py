f009a1d61cc32e6f95185fee748df1ecf164838e
Hualet Wang <mr.asianwang@gmail.com>
2016-02-14 10:40:36 +0800

    improve copyright
    Change-Id: Ib34ab1cf0653e8250155bbadd8df9f9959f1c05e

--- src/main.py
+++ src/main.py
@@ -1,26 +1,12 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# Copyright (C) 2011 ~ 2013 Deepin, Inc.
-#               2011 ~ 2012 Wang Yong
+# Copyright (C) 2013 Deepin Technology Co., Ltd.
 #
-# Author:     Wang Yong <lazycat.manatee@gmail.com>
-# Maintainer: Wang Yong <lazycat.manatee@gmail.com>
-#             Yueqian Zhang <nohappiness@gmail.com>
-#             Hualet Wang <mr.asianwang@gmail.com>
-#
-# This program is free software: you can redistribute it and/or modify
+# This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
 
 from collections import OrderedDict
 from contextlib import contextmanager
