c04b8a3f9bd218b00b324761aa3f92dcabba8bc1
Andy Stewart <lazycat.manatee@gmail.com>
2016-08-23 12:26:09 +0800

    Adjust update_mo.sh make sure update system's mo files.

--- install_script/update_mo.sh
+++ install_script/update_mo.sh
@@ -2,6 +2,7 @@
 for lang in po/*
 do
     mkdir -p ${DESTDIR}${PREFIX}/share/locale/$(basename ${lang})/LC_MESSAGES/
+    rm -f ${DESTDIR}${PREFIX}/share/locale/$(basename ${lang})/LC_MESSAGES/deepin-terminal.mo
 	msgfmt --output ${DESTDIR}${PREFIX}/share/locale/$(basename ${lang})/LC_MESSAGES/deepin-terminal.mo po/$(basename ${lang})/LC_MESSAGES/deepin-terminal.po
 done	
 	
