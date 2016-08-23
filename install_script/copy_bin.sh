664f44b6fb9b4dad993488feef7af4c481e2d7f2
Andy Stewart <lazycat.manatee@gmail.com>
2016-08-23 13:10:14 +0800

    Rest copy_bin.sh copy path.

--- install_script/copy_bin.sh
+++ install_script/copy_bin.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-cp -fr ../main ${DESTDIR}${PREFIX}/share/deepin-terminal
+cp -fr main ${DESTDIR}${PREFIX}/share/deepin-terminal
 ln -sf ${PREFIX}/share/deepin-terminal/main ${DESTDIR}${PREFIX}/bin/deepin-terminal
