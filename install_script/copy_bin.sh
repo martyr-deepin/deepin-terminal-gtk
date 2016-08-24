12f3f78d3227c0543d4ace2dd6550234e7390646
Andy Stewart <lazycat.manatee@gmail.com>
2016-08-24 14:12:40 +0800

    Add IBUS_NO_SNOOPER_APPS option wrap main, avoid Ctrl + 5 eat by fctix.

--- install_script/copy_bin.sh
+++ install_script/copy_bin.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 install -p -D -m 0755 main ${DESTDIR}${PREFIX}/share/deepin-terminal
-ln -sf ${PREFIX}/share/deepin-terminal/main ${DESTDIR}${PREFIX}/bin/deepin-terminal
+install -p -D -m 0755 deepin-terminal ${DESTDIR}${PREFIX}/share/deepin-terminal
+ln -sf ${PREFIX}/share/deepin-terminal/deepin-terminal ${DESTDIR}${PREFIX}/bin/deepin-terminal
