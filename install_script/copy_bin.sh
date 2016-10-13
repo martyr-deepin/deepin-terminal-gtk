48553bf77b713ef86f06d347a6396b26796c349d
Andy Stewart <lazycat.manatee@gmail.com>
2016-10-13 16:12:24 +0800

    Don't need install_script since CMake have do those jobs.

--- install_script/copy_bin.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-install -p -D -m 0755 main ${DESTDIR}${PREFIX}/share/deepin-terminal
-ln -sf ${PREFIX}/share/deepin-terminal/main ${DESTDIR}${PREFIX}/bin/deepin-terminal
--- install_script/update_mo.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-for lang in po/*
-do
-    install -d ${DESTDIR}${PREFIX}/share/locale/$(basename ${lang})/LC_MESSAGES/
-    rm -f ${DESTDIR}${PREFIX}/share/locale/$(basename ${lang})/LC_MESSAGES/deepin-terminal.mo
-	msgfmt --output ${DESTDIR}${PREFIX}/share/locale/$(basename ${lang})/LC_MESSAGES/deepin-terminal.mo po/$(basename ${lang})/LC_MESSAGES/deepin-terminal.po
-done	
-	
